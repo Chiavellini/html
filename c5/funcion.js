1a5112f2594cf65961b0fd69b687442e1ac27e0f
Bernardo Del rio <bernardodelrio@Bernardos-MacBook-Air-2.local>
2025-09-19 09:56:08 -0600

    agregar clase 5

---
+++ c5/funcion.js
@@ -0,0 +1,26 @@
+function multiplicacionC(a, b) {
+  return a * b;
+}
+console.log(multiplicacionC(4, 5)); 
+
+
+const multiplicacionA = (a, b) => {
+  return a * b;
+}
+console.log(multiplicacionA(4, 5)); 
+
+
+function cuadradoC(x) {
+  return x * x;
+}
+console.log(cuadradoC(5)); 
+
+
+const cuadradoA = (x) => {
+  return x * x;
+}
+console.log(cuadradoA(5)); 
+
+
+
+
